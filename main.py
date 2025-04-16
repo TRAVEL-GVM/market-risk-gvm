acaa87f88fe59208e1ea855c882e0bde5a6340bd
TRAVEL <ricardo.oliveira.marques@novobanco.pt>
2025-04-16 15:18:13 +0000

    ..

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="S&P 500", layout="wide")
 
 @st.cache_data
 def load_data():
-    df = pd.read_csv("dados/sp500.csv", parse_dates=["Date"])
+    df = pd.read_csv("dados/sp500.csv")
     return df
 
 st.title("📈 S&P 500 - Dados Yahoo Finance")
